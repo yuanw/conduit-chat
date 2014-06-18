d6e46b8ba2f2907099b97901bc410dde0b739a7d
Yuan <ywang@vendasta.com>
2014-06-17 21:26:06 -0600

    update doc

--- server.hs
+++ server.hs
@@ -1,3 +1,4 @@
+--https://www.fpcomplete.com/user/joehillen/building-an-async-chat-server-with-conduit
 {-# LANGUAGE OverloadedStrings #-}
 
 import Control.Monad.Trans (liftIO)
@@ -6,5 +7,7 @@ import Data.Conduit.Network
 import qualified Data.ByteString.Char8 as BS
 
 main :: IO ()
+--IsString instance should be using the extension OverloadedStrings--
+-- runTCPServer :: (MonadIO m, MonadBaseControl IO m) => Application m -> m()
 main = runTCPServer (serverSettings 4000 "*") $ \appData ->
     appSource appData $$ awaitForever $ liftIO . BS.putStr
